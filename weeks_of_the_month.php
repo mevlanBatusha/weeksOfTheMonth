86cd173c54a8fd2c89398cab7018a755d582802c
Mevlan Batusha <deco_lani@hotmail.com>
2017-03-04 18:50:41 +0100

    .

---
+++ weeks_of_the_month.php
@@ -0,0 +1,34 @@
+<?php
+
+// get date
+$date = date('d/m/Y');
+
+list($send_day, $send_month, $send_year) = explode('/',$date);
+
+// send values to function and get the result
+$result = getWeeks($send_day, $send_month, $send_year);
+
+function getWeeks($getDay, $getMonth, $getYear)
+{
+    // remove 0 from getDay
+    $i = 0 + $getDay;
+
+    $getDate = $getYear.'/'.$getDay.'/'.$getMonth;
+    
+    // take days of the month selected
+    $days_of_month = date('t', strtotime($getDate));
+
+    $rez = array();
+    $j = 0;
+    while ( $i < $days_of_month) 
+    {
+        $rez[$j]= $days_of_month - $i;
+        //increes variable of j for 1 for array and i for 7 "days"
+        $j++;
+        $i = $i + 7;
+    }
+    // count all elemnt in an array
+    $weeks_of_month = count($rez);
+
+    return $weeks_of_month;
+}
